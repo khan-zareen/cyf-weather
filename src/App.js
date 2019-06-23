1d4aae70d90fec3f398816eb662c8b0b6a8f2c98
zareen <evocativevict@gmail.com>
2019-06-23 13:45:16 +0100

    link between CSS and Html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { FINDWEATHER } from "./component/search";
-import { Search, Image } from "./component/search";
-import cloudy from "./img/weather-icons/cloudy.svg";
 import React, { Component } from "react";
 import "./App.css";
+import storm from "./img/weather-icons/storm.svg";
 
 class App extends Component {
   constructor(props) {
@@ -12,68 +10,88 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <FINDWEATHER />
-        <Search />
-        <Image src={cloudy} />
+      <div className="app">
+        <header className="app__header app-header">
+          FIND WEATHER<div>london</div>
+        </header>
+        <main className="app__main app_main">
+          <div className="overcast">
+            <div>
+              <img src={storm} alt={"stormy weather"} />
+            </div>
+
+            <h1>overcast clouds</h1>
+          </div>
+
+          <div className="temp">
+            <h2>Temperature 10 &#176; to 11 &#176;C </h2>
+          </div>
+
+          <div className="humidity_pressure">
+            <p>Humidity</p>
+            <p>78%</p>
+            <p>Pressure</p>
+            <p>1008.48</p>
+          </div>
+
+          <div className="twentyfour_hours">
+            <div>
+              <p>03:00</p>
+
+              <div className="icon">
+                <img src={storm} alt="stormy weather" />
+              </div>
+              <p> 8 &#176;C</p>
+            </div>
+
+            <div>
+              <p>06:00</p>
+
+              <div className="icon">
+                <img src={storm} alt="stormy weather" />
+              </div>
+
+              <p> 9 &#176;C</p>
+            </div>
+
+            <div>
+              <p>09:00</p>
+              <div className="icon">
+                <img src={storm} alt="stormy weather" />
+              </div>
+              <p> 14 &#176;C</p>
+            </div>
+
+            <div>
+              <p>12:00</p>
+              <div className="icon">
+                <img src={storm} alt="stormy weather" />
+              </div>
+              <p> 17 &#176;C</p>
+            </div>
+
+            <div>
+              <p>15:00</p>
+              <div className="icon">
+                <img src={storm} alt="stormy weather" />
+              </div>
+              <p> 18 &#176;C</p>
+            </div>
+
+            <div>
+              <p>18:00</p>
+              <img src={storm} alt="stormy weather" />
+              <p> 16 &#176;C</p>
+            </div>
+
+            <div>
+              <p>21:00</p>
+              <img src={storm} alt="stormy weather" />
+              <p> 13 &#176;C</p>
+            </div>
+          </div>
+        </main>
       </div>
-      /* // <div className="app">
-      // <header className="Zareenweather_app">
-      //     FIND WEATHER<div>london</div>
-      //   </header>
-      //   <main className="app__main">
-      //     <div>
-      //       <img src={storm} alt={"stormy weather"} />
-      //       <div>
-      //         <h1>overcast clouds</h1>
-      //         <div>
-      //           <h2>Temperature 10 &#176; to 11 &#176;C </h2>
-      //           <div>
-      //             <p>Humidity</p>
-      //             <p>78%</p>
-      //             <p>Pressure</p>
-      //             <p>1008.48</p>
-      //             <div>
-      //               <p>03:00</p>
-      //               <img src={storm} alt="stormy weather" />
-      //               <p> 8 &#176;C</p>
-      //             </div>
-      //             <div>
-      //               <p>06:00</p>
-      //               <img src={storm} alt="stormy weather" />
-      //               <p> 9 &#176;C</p>
-      //             </div>
-      //             <div>
-      //               <p>09:00</p>
-      //               <img src={storm} alt="stormy weather" />
-      //               <p> 14 &#176;C</p>
-      //             </div>
-      //             <div>
-      //               <p>12:00</p>
-      //               <img src={storm} alt="stormy weather" />
-      //               <p> 17 &#176;C</p>
-      //             </div>
-      //             <div>
-      //               <p>15:00</p>
-      //               <img src={storm} alt="stormy weather" />
-      //               <p> 18 &#176;C</p>
-      //             </div>
-      //             <div>
-      //               <p>18:00</p>
-      //               <img src={storm} alt="stormy weather" />
-      //               <p> 16 &#176;C</p>
-      //             </div>
-      //             <div>
-      //               <p>21:00</p>
-      //               <img src={storm} alt="stormy weather" />
-      //               <p> 13 &#176;C</p>
-      //             </div>
-      //           </div>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </main>
-      // </div> */
     );
   }
 }
